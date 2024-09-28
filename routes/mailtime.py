ad98189e25b881ba29c308dc084f7df50f8b86fd
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-09-29 00:27:43 +0800

    mailtime

--- routes/mailtime.py
+++ routes/mailtime.py
@@ -14,9 +14,6 @@ class Email:
         self.time = int(datetime.fromisoformat(email_json["timeSent"]).timestamp())
         self.sender = email_json["sender"]
         self.receiver = email_json["receiver"]
-    
-    def __str__(self):
-        return f"{self.subject} {self.time} {self.sender} {self.receiver}"
 
 @app.route('/mailtime', methods=['POST'])
 def eval_mailtime():
@@ -35,24 +32,13 @@ def eval_mailtime():
                 people[responder_email.sender].append(responder_email.time - graph[subject][i-1].time)
 
         result = {}
-        for person in people:
-            result[person] = sum(people[person])//len(people[person])
+        for person_obj in request_json["users"]:
+            person = person_obj["name"]
+            if len(people[person]) == 0:
+                result[person] = 0
+            else:
+                result[person] = sum(people[person])//len(people[person])
 
         return {"response": result}
-
-    # def solve(request_json):
-    #     for email in request_json["emails"]:
-    #         subject = email["subject"].split("RE: ")[-1]
-    #         graph[subject].append(
-    #             int(datetime.fromisoformat(email["timeSent"]).timestamp())
-    #         )
-    #         if len(graph[subject]) > 1:
-    #             people[email["sender"]].append(
-    #                 graph[subject][-1] - graph[subject][-2])
-
-    #     result = {}
-    #     for person in people:
-    #         result[person] = sum(people[person]) // len(people[person])
-    #     return result
 
     return json.dumps(solve(request.get_json()))
