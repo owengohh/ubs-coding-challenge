6794061316706af1ba3bced57208d08926d44a91
onebignick <nicholas.zhengkai.ong@gmail.com>
2024-09-28 21:40:15 +0800

    mailtime

--- routes/mailtime.py
+++ routes/mailtime.py
@@ -8,6 +8,15 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
+class Email:
+    def __init__(self, email_json):
+        self.subject = email_json["subject"].split("RE: ")[-1]
+        self.time = int(datetime.fromisoformat(email_json["timeSent"]).timestamp())
+        self.sender = email_json["sender"]
+        self.receiver = email_json["receiver"]
+    
+    def __str__(self):
+        return f"{self.subject} {self.time} {self.sender} {self.receiver}"
 
 @app.route('/mailtime', methods=['POST'])
 def eval_mailtime():
@@ -16,19 +25,34 @@ def eval_mailtime():
 
     def solve(request_json):
         for email in request_json["emails"]:
-            subject = email["subject"].split("RE: ")[-1]
-            graph[subject].append(
-                int(datetime.fromisoformat(email["timeSent"]).timestamp())
-            )
-            if len(graph[subject]) > 1:
-                people[email["sender"]].append(
-                    graph[subject][-1] - graph[subject][-2])
+            new_email = Email(email)
+            graph[new_email.subject].append(new_email)
+        
+        for subject in graph:
+            graph[subject].sort(key = lambda x:x.time)
+            for i in range(1, len(graph[subject])):
+                responder_email = graph[subject][i]
+                people[responder_email.sender].append(responder_email.time - graph[subject][i-1].time)
 
         result = {}
         for person in people:
-            result[person] = sum(people[person]) // len(people[person])
-        return result
+            result[person] = sum(people[person])//len(people[person])
 
-    return json.dumps({
-        "response": solve(request.get_json())
-    })
+        return {"response": result}
+
+    # def solve(request_json):
+    #     for email in request_json["emails"]:
+    #         subject = email["subject"].split("RE: ")[-1]
+    #         graph[subject].append(
+    #             int(datetime.fromisoformat(email["timeSent"]).timestamp())
+    #         )
+    #         if len(graph[subject]) > 1:
+    #             people[email["sender"]].append(
+    #                 graph[subject][-1] - graph[subject][-2])
+
+    #     result = {}
+    #     for person in people:
+    #         result[person] = sum(people[person]) // len(people[person])
+    #     return result
+
+    return json.dumps(solve(request.get_json()))
